c8c95bfa97b4de333164274fb57d49d022f919bb
Daniel França <danieldrafts@gmail.com>
2021-02-23 18:34:09 -0300

    commit inicial

---
+++ Enum/Genero.cs
@@ -0,0 +1,13 @@
+namespace Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Terror = 13
+    }
+}
